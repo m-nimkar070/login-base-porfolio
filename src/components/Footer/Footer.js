9594ab42719cf3bc1486590d130e1106eea119d2
Mahesh Nimkar <132036650+m-nimkar070@users.noreply.github.com>
2023-12-02 19:30:52 +0000

    feat: Update Footer and Portfolio components
    Updated Footer and Portfolio components to improve styling and functionality

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,34 +1,73 @@
-import React from 'react';
-import style from "./footer.module.css"
+import React from "react";
+import style from "./footer.module.css";
 import gitLogo from "../../Assets/icons8-github-40.png";
 import twitLogo from "../../Assets/icons8-twitter-40.png";
 import linkLogo from "../../Assets/icons8-linkedin-40.png";
-import trademark from "../../Assets/icons8-trademark-40.png"
+import trademark from "../../Assets/icons8-trademark-40.png";
+import { Grid, Typography, Container, Link } from "@mui/material";
 
 const Footer = () => {
   return (
-    <div className={style.container}>
-    <div className={style.footer}>
-      <div className={style.row}>
-        <div className={style.col}>
-            <p>Address: <span>Bengaluru, Karnataka 560045</span></p>
-            <p>Something Co.<img className={style.trademark} src={trademark} alt="trademark" /></p>
-        </div>
-      </div>
-      <div className={style.row}>
-        <div className={style.col}>
-            <a href='#download' id='about'><p>About</p></a>
-        </div>
-        <div className={style.column}>
-            <p>Connect with me</p>
-            <a href='https://www.linkedin.com/in/mahesh-nimkar/'><img className={style.imgcol} src={linkLogo} alt="LinkedIn logo" /></a>
-            <a href='https://twitter.com/?lang=en-in'><img className={style.imgcol} src={twitLogo} alt="Twitter logo" /></a>
-            <a href='https://github.com/m-nimkar070'><img className={style.imgcol} src={gitLogo} alt="GitHub logo" /></a>
-        </div>
-      </div>
-      </div>
-    </div>
-  )
-}
+    <Grid item className={style.container}>
+      <Container container className={style.main} xs={12} sm={12} spacing={3}>
+        <Grid item xs={12} p={1}>
+          <Typography variant="h6">
+            Something Co.
+            <img
+              src={trademark}
+              style={{ width: "20px", height: "20px" }}
+              alt="trademark"
+            />
+          </Typography>
+          <Typography variant="body2">
+            Address: Bengaluru, Karnataka 560045
+          </Typography>
+          <Link href="#about">
+            <Typography variant="body2">About</Typography>
+          </Link>
+        </Grid>
+        <Grid item xs={12} md={6} p={1}>
+          <Typography variant="body2">Connect with me</Typography>
+          <Link
+            className={style.link}
+            href="https://www.linkedin.com/in/mahesh-nimkar/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img
+              src={linkLogo}
+              alt="LinkedIn logo"
+              style={{ width: "40px", height: "40px" }}
+            />
+          </Link>
+          <Link
+            className={style.link}
+            href="#"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img
+              src={twitLogo}
+              alt="Twitter logo"
+              style={{ width: "40px", height: "40px" }}
+            />
+          </Link>
+          <Link
+            className={style.link}
+            href="https://github.com/m-nimkar070"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img
+              src={gitLogo}
+              alt="GitHub logo"
+              style={{ width: "40px", height: "40px" }}
+            />
+          </Link>
+        </Grid>
+      </Container>
+    </Grid>
+  );
+};
 
 export default Footer;
--- src/components/Portfolio/Portfolio.js
+++ src/components/Portfolio/Portfolio.js
@@ -1,11 +1,18 @@
-import React from 'react';
-import { useAppContext } from '../Context/AppContext';
+import React from "react";
+import { useAppContext } from "../Context/AppContext";
 import style from "./portfolio.module.css";
-import client from "../../Assets/client.jpg"
+import client from "../../Assets/client.jpg";
 import gif from "../../Assets/thankyou.gif";
-import Footer from '../Footer/Footer';
-import { Grid, Typography, Button, Box, Card, CardContent, CardMedia } from "@mui/material";
-
+import Footer from "../Footer/Footer";
+import {
+  Grid,
+  Typography,
+  Button,
+  Box,
+  Card,
+  CardContent,
+  CardMedia,
+} from "@mui/material";
 
 const Portfolio = () => {
   const { handleLogout, data } = useAppContext();
@@ -30,66 +37,99 @@ const Portfolio = () => {
 
   return (
     <Box>
-    <Grid>
-         <Grid item xs={12} sx={{ borderRadius: '16px' }} className={style.nav}>
-         <Typography variant="h3">Welcome {data[0].name}</Typography>
-         <Button className={style.btn} sx={[{color:"white"}]} p={2}   onClick={handleLogout}>
-           Logout
-         </Button>
-       </Grid>
-    </Grid>
-    <Grid>
-        <Grid container item xs={12} spacing={2} >
-         <Grid item xs={12} className={style.details}>
-           <Typography variant="h3" className={style.h1}>
-             I'm {data[0].name.split(" ")[0]}{' '}
-             <span className={style.span} >{data[0].name.split(" ")[1]}</span>
-           </Typography>
-           <Typography className={style.p}>
-             This is my portfolio To showcase my<br />Skills in Front-End Technologies
-           </Typography>
-           <a href={gif}  id='download' download className={style.a}>
-             Download CV
-           </a>
-         </Grid>
-       </Grid>
-    </Grid>
-    <Grid item xs={12} md={12} lg={12}>
-        <Typography align='center'>Following are some of my clients with their details ...!</Typography>
-    </Grid>
-    <Grid container className={style.cardContainer} marginY="1rem" paddingX="1rem" xs={12} spacing={2}>
-      {slicedData.map((item) => (
-        <Grid item xs={12} sm={6} md={6} lg={4} key={item.id}>
-          <Card className="card" >
-              <CardMedia component="img" alt={item.name} image={client}/>
-                <CardContent className={style.card}>
-                  <Grid className={style.cardContent}>
-                    <Typography variant="subtitle1">Name:</Typography>
-                    <Typography variant="body1">{item.name}</Typography>
-                  </Grid>
-                  <Grid className={style.cardContent}>
-                    <Typography variant="subtitle1">Email:</Typography>
-                    <Typography variant="body1">{item.email}</Typography>
-                  </Grid>
-                  <Grid className={style.cardContent}>
-                    <Typography variant="subtitle1">Website:</Typography>
-                    <Typography variant="body1">{item.website}</Typography>
-                  </Grid>
-                  <Grid className={style.cardContent}>
-                    <Typography variant="subtitle1">Company:</Typography>
-                    <Typography variant="body1">{item.company.name}</Typography>
-                  </Grid>
-                </CardContent>
-            </Card>
+      <Grid>
+        <Grid item xs={12} sx={{ borderRadius: "16px" }} className={style.nav}>
+          <Typography variant="h3">Welcome {data[0].name}</Typography>
+          <Button
+            className={style.btn}
+            sx={[
+              {
+                color: "white",
+                border: "1px solid white",
+                borderRadius: "20px",
+                margin: "5px",
+              },
+            ]}
+            p={2}
+            onClick={handleLogout}
+          >
+            Logout
+          </Button>
         </Grid>
-      ))}
-    </Grid>
-    <Grid container className={style.cardContainer} marginY="1rem" paddingX="1rem" xs={12} spacing={2}>
-
+      </Grid>
+      <Grid>
+        <Grid container item xs={12} spacing={2}>
+          <Grid item xs={12} className={style.details}>
+            <Typography variant="h3" className={style.h1}>
+              I'm {data[0].name.split(" ")[0]}{" "}
+              <span className={style.span}>{data[0].name.split(" ")[1]}</span>
+            </Typography>
+            <Typography className={style.p}>
+              This is my portfolio To showcase my
+              <br />
+              Skills in Front-End Technologies
+            </Typography>
+            <a href={gif} id="download" download className={style.a}>
+              Download CV
+            </a>
+          </Grid>
+        </Grid>
+      </Grid>
+      <Grid item xs={12} md={12} lg={12}>
+        <Typography align="center">
+          Following are some of my clients with their details ...!
+        </Typography>
+      </Grid>
+      <Grid
+        container
+        className={style.cardContainer}
+        marginY="1rem"
+        paddingX="1rem"
+        xs={12}
+        spacing={2}
+      >
+        {slicedData.map((item) => (
+          <Grid item xs={12} sm={6} md={6} lg={4} key={item.id}>
+            <Card className="card">
+              <CardMedia
+                component="img"
+                alt={item.name}
+                sx={{ borderRadius: "10px" }}
+                image={client}
+              />
+              <CardContent className={style.card}>
+                <Grid className={style.cardContent}>
+                  <Typography variant="subtitle1">Name:</Typography>
+                  <Typography variant="body1">{item.name}</Typography>
+                </Grid>
+                <Grid className={style.cardContent}>
+                  <Typography variant="subtitle1">Email:</Typography>
+                  <Typography variant="body1">{item.email}</Typography>
+                </Grid>
+                <Grid className={style.cardContent}>
+                  <Typography variant="subtitle1">Website:</Typography>
+                  <Typography variant="body1">{item.website}</Typography>
+                </Grid>
+                <Grid className={style.cardContent}>
+                  <Typography variant="subtitle1">Company:</Typography>
+                  <Typography variant="body1">{item.company.name}</Typography>
+                </Grid>
+              </CardContent>
+            </Card>
+          </Grid>
+        ))}
+      </Grid>
+      <Grid
+        container
+        className={style.cardContainer}
+        marginY="1rem"
+        paddingX="1rem"
+        xs={12}
+        spacing={2}
+      >
         <Footer />
-
-    </Grid>
-  </Box>
+      </Grid>
+    </Box>
   );
 };
 
